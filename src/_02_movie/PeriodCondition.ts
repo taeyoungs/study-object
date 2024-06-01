0b2c78d58d79a3852e7b5c0976735eb0b99af96c
taeyoungs <xoxodudwkd@gmail.com>
2024-06-01 21:24:03 +0900

    할인 조건을 추가하라.
    - DiscountCondition 인터페이스
    - DiscountCondition 인터페이스를 구현하는 PeriodCondition 클래스
    - DiscountCondition 인터페이스를 구현하는 SequenceCondition 클래스

---
+++ src/_02_movie/PeriodCondition.ts
@@ -0,0 +1,29 @@
+import { compareAsc } from 'date-fns';
+
+import { DiscountCondition } from './DiscountCondition';
+
+type DayOfWeek = '0' | '1' | '2' | '3' | '4' | '5' | '6';
+
+// 기간 할인 조건
+export class PeriodCondition implements DiscountCondition {
+  #dayOfWeek: DayOfWeek;
+  #startTime: Date;
+  #endTime: Date;
+
+  constructor(dayOfWeek: DayOfWeek, startTime: Date, endTime: Date) {
+    this.#dayOfWeek = dayOfWeek;
+    this.#startTime = startTime;
+    this.#endTime = endTime;
+  }
+
+  // Screening의 상영 요일이 dayOfWeek와 같고 상영 시작 시간이
+  // startTime과 endTime 사이에 있을 경우에는 true를 반환하고,
+  // 그렇지 않은 경우에는 false를 반환한다.
+  isSatisfiedBy(screening: Screening) {
+    return (
+      screening.startTime.getDay() === this.#dayOfWeek &&
+      compareAsc(this.#startTime, screening.startTime) <= 0 &&
+      compareAsc(this.#endTime, screening.startTime) >= 0
+    );
+  }
+}
---
+++ src/_02_movie/SequenceCondition.ts
@@ -0,0 +1,15 @@
+import { DiscountCondition } from './DiscountCondition';
+
+// 순번 할인 조건
+export class SequenceCondition implements DiscountCondition {
+  #sequence: number;
+
+  constructor(sequence: number) {
+    this.#sequence = sequence;
+  }
+
+  // Screening의 상영 순번과 일치할 경우 할인 가능한 것으로 판단
+  isSatisfiedBy(screening: Screening) {
+    return screening.isSequence(this.#sequence);
+  }
+}
