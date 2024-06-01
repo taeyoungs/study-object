202fe2817bf13ffad8371916f29484b1f6ca6df6
taeyoungs <xoxodudwkd@gmail.com>
2024-06-01 21:46:21 +0900

    Screening 클래스로 상영 정보를 표현하라.

---
+++ src/_02_movie/Screening.ts
@@ -0,0 +1,33 @@
+import { Customer } from './Customer';
+
+export class Screening {
+  #movie: Movie;
+  #sequence: number;
+  #whenScreened: Date;
+
+  constructor(movie: Movie, sequence: number, whenScreened: Date) {
+    this.#movie = movie;
+    this.#sequence = sequence;
+    this.#whenScreened = whenScreened;
+  }
+
+  getStartTime() {
+    return this.#whenScreened;
+  }
+
+  isSequence(sequence: number) {
+    return this.#sequence === sequence;
+  }
+
+  getMovieFee() {
+    this.#movie.fee;
+  }
+
+  calculateFee(audienceCount: number) {
+    return this.#movie.calcalateFee(this).times(audienceCount);
+  }
+
+  reserve(customer: Customer, audienceCount: number) {
+    return new Reservation(customer, this, this.calculateFee(audienceCount), audienceCount);
+  }
+}
